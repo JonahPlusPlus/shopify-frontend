0045c9d929066665f35301dd62ad5b5c7252c61b
Jonah Henriksson <33059163+JonahPlusPlus@users.noreply.github.com>
2022-05-22 11:12:41 -0400

    Updated some features

--- static/main.js
+++ static/main.js
@@ -51,7 +51,7 @@ function queryAI() {
     fetch("/query", {
         method: 'post',
         headers: new Headers({'Content-Type': 'application/json'}),
-        body: JSON.stringify({ prompt: conversation, temperature: $("#temp").val()/10.0, max_tokens: 60, frequency_penalty: 0.5, presence_penalty: 0.2, stop: ["Human:", "Human"] }),
+        body: JSON.stringify({ prompt: conversation, temperature: $("#temp").val()/10.0, max_tokens: 120, frequency_penalty: 0.5, presence_penalty: 0.2, stop: ["Human:", "Human"] }),
         mode: "no-cors"
     }).then(function(response){
         return response.json();
@@ -61,7 +61,7 @@ function queryAI() {
         var ai_message = $("<div>").text(ai_response).addClass("ai");
 
         $("#conversation").append(ai_message);
-    });
 
-    $("#conversation").scrollTop($('#conversation')[0].scrollHeight);
+        $("#conversation").scrollTop($('#conversation')[0].scrollHeight);
+    });
 }
